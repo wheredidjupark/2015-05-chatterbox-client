3cd647b5cb988e7080dcd314c49c331703d43299
Ju Hyeon Park <wheredidjupark@gmail.com>
2015-06-01 23:07:18 -0700

    fix the prompt bug

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,30 +1,49 @@
 // YOUR CODE HERE:
 
 
+
+
+$(document).ready(function() {
+
+
+    app.init();
+
+});
+
+var appData = {
+    username: null,
+    roomname: null,
+    messages: null,
+    friends: {}
+};
+
+
 var app = {
-    "username": null,
-    "roomname": null,
+
+
+    //where the app's functionality is organized 
     "init": function() {
-        app.username = prompt("What is your username?") || "Anonymous";
+    	appData.username = window.location.search.substr(10);
         app.get();
         setInterval(app.get, 2000);
-        //app.roomChange(app.messages);
+        //app.roomChange(appData.messages);
         app.addFriend();
         app.submit();
     },
-    "messages": null,
-    //update the chatterbox app with latest messages (only once)
+
+    //update the chatterbox app with latest messages stored as an array of message objects
     "update": function(messages) {
         $("#messages").html("");
         _.each(messages, function(msg) {
             //convert each msg into a container
-            if (!app.roomname) {
+            if (!appData.roomname) {
                 $("#messages").append(messagePackager(msg));
-            } else if (app.roomname === msg.roomname) {
+            } else if (appData.roomname === msg.roomname) {
                 $("#messages").append(messagePackager(msg));
             }
         });
     },
+    //gets messages from the server and updates the app to include latest messages
     "get": function() {
         $.ajax({
             url: 'https://api.parse.com/1/classes/chatterbox',
@@ -33,8 +52,8 @@ var app = {
                 console.log('chatterbox: Message retrieved');
                 //console.log(data); //look at the data retrieved
                 //upon success, update the application.
-                app.messages = data.results;
-                app.update(app.messages);
+                appData.messages = data.results;
+                app.update(appData.messages);
             },
             error: function(data) {
                 // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -42,64 +61,86 @@ var app = {
             }
         });
     },
-    "friends": {},
+    //adds a friend upon clicking username
     "addFriend": function() {
 
         $("#messages").on("click", ".messageUser", function(event) {
             event.preventDefault();
             var friendName = $(this).text(); //a way to get the username
-            app.friends[friendName] = !app.friends[friendName];
-            console.log("successfully added");
+            appData.friends[friendName] = !appData.friends[friendName];
+            if (appData.friends[friendName]) {
+                console.log("successfully added friend");
+            } else {
+                console.log("successfully removed friend");
+            }
+
         });
     },
+    //submits and posts form to the server after the form is converted into a message
     "submit": function() {
         $("#main").on("click", ".submit", function(event) {
             event.preventDefault();
-            //var text = $(".text").val();
 
             var msg = {
-                "username": app.username,
+                "username": appData.username,
                 "text": $(".text").val(),
                 "roomname": app.currentRoom
             };
-            post(msg);
+            app.post(msg);
             app.get();
             $(".text").val("");
         });
-    }/*
-    ,
-    "roomChange": function(messages) {
-
-        var rooms = {};
-
-
-
-        $("#main").on("change", "#rooms", function(event) {
-
-            if (this.value === 0) {
-                var newroom = prompt("Please provide name of the newly created room");
-                app.roomname = newroom;
-                var $option = $(" <option value=" + newroom + ">" + newroom + " < /option>");
-                $("#rooms").append($option);
-            } else {
-                app.roomname = this.value;
+    },
+    //posts message to the server
+    "post": function(msg) {
+        $.ajax({
+            url: "https://api.parse.com/1/classes/chatterbox",
+            type: "POST",
+            data: JSON.stringify(msg),
+            contentTYPE: "application/json",
+            success: function(data) {
+                console.log("chatterbox: Message sent successfully");
+            },
+            error: function(data) {
+                console.error("chatterbox: Failed to send message");
             }
-
-            _.each(messages, function(message) {
-
-                if (!rooms[message.roomname]) {
-                    rooms[message.roomname] = true;
-                    var $room = $(" <option value=" + room + ">" + room + " < /option>");
-                    $("#rooms").append($room);
-                }
-            });
-
-           console.log("Here's the updated rooms",rooms);
-
-
         });
     }
-*/
+
+    /*
+        ,
+        "roomChange": function(messages) {
+
+            var rooms = {};
+
+
+
+            $("#main").on("change", "#rooms", function(event) {
+
+                if (this.value === 0) {
+                    var newroom = prompt("Please provide name of the newly created room");
+                    appData.roomname = newroom;
+                    var $option = $(" <option value=" + newroom + ">" + newroom + " < /option>");
+                    $("#rooms").append($option);
+                } else {
+                    appData.roomname = this.value;
+                }
+
+                _.each(messages, function(message) {
+
+                    if (!rooms[message.roomname]) {
+                        rooms[message.roomname] = true;
+                        var $room = $(" <option value=" + room + ">" + room + " < /option>");
+                        $("#rooms").append($room);
+                    }
+                });
+
+               console.log("Here's the updated rooms",rooms);
+
+
+            });
+        }
+    */
 };
 
 //convert msg into a proper HTML container
@@ -114,30 +155,5 @@ var messagePackager = function(msg) {
     return $message;
 };
 
-var post = function(msg) {
-    $.ajax({
-        url: "https://api.parse.com/1/classes/chatterbox",
-        type: "POST",
-        data: JSON.stringify(msg),
-        contentTYPE: "application/json",
-        success: function(data) {
-            console.log("chatterbox: Message sent successfully");
-        },
-        error: function(data) {
-            console.error("chatterbox: Failed to send message");
-        }
-    });
-};
-
-//features we need:
-//clear the page whenever we fetch new messages
 
 
-$(document).ready(function() {
-    app.init();
-
-});
-
-
-
-//setInterval( retrieve, 5000);
